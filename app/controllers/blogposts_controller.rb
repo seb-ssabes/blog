faeb273870debc6c4ca3cbbf3ffe075e65093f1f
Sebastian Olivares <olivares.heredia@gmail.com>
2024-05-02 11:38:01 -0600

    wrapping up basic blogpost controller

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,14 +1,11 @@
 class BlogpostsController < ApplicationController
-  def initialize
-
-  end
+  before_action :set_blogpost, except: [:index, :new, :create ] # or only: [:show, :edit, :update, :destroy]
 
   def index
     @blogposts = Blogpost.all
   end
 
   def show
-    @blogpost = Blogpost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
@@ -26,9 +23,31 @@ class BlogpostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @blogpost.update(blogpost_params)
+      redirect_to @blogpost
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blogpost.destroy
+    redirect_to root_path
+  end
+
   private
 
   def blogpost_params
     params.require(:blogpost).permit(:title, :body)
+  end
+
+  def set_blogpost
+    @blogpost = Blogpost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  get "/blogposts/new", to: "blogposts#new", as: :new_blogpost
-  get "/blogposts/:id", to: "blogposts#show", as: :blogpost
-  post "/blogposts", to: "blogposts#create", as: :blogposts
-
+  resources :blogposts
   root "blogposts#index"
 end
