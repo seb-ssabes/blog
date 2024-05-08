520267e164feb675e3c96cc0dc46b93dbb1d367e
Sebastian Olivares <olivares.heredia@gmail.com>
2024-05-08 12:28:23 -0400

    adding test for blogposts methods

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -12,7 +12,7 @@ class Blogpost < ApplicationRecord
   end
 
   def published?
-    published_at? && published_at? <= Time.current
+    published_at? && published_at <= Time.current
   end
 
   def scheduled?
--- test/models/blogpost_test.rb
+++ test/models/blogpost_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class BlogpostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blogpost" do
+    assert blogposts(:draft).draft?
+  end
+
+  test "draft? returns false for a published blogpost" do
+    refute blogposts(:published).draft?
+  end
+
+  test "draft? returns false for a scheduled blogpost" do
+    refute blogposts(:scheduled).draft?
+  end
+
+  test "published? returns true for a published blogpost" do
+    assert blogposts(:published).published?
+  end
+
+  test "published? returns false for a draft blogpost" do
+    refute blogposts(:draft).published?
+  end
+
+  test "published? returns false for a scheduled blogpost" do
+    refute blogposts(:scheduled).published?
+  end
+
+  test "scheduled? returns true for a scheduled blogpost" do
+    assert blogposts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for a draft blogpost" do
+    refute blogposts(:draft).scheduled?
+  end
+
+  test "scheduled? returns false for a published blogpost" do
+    refute blogposts(:published).scheduled?
+  end
 end
